92ef1b552daaf1c4f015c531e6d8f03fb4591676
jyang825 <jyang825@76eb3cc0-9619-0410-aeca-853053af2ac4>
2009-06-08 23:35:47 +0000

    Added cancel. Moved common functions to util.

--- 4/js/jquery-ui-table.js
+++ 4/js/jquery-ui-table.js
@@ -8,101 +8,142 @@
 
 (function($) {
 
-	var prototype = {
+  var util = {
+    addHover: function(e) {
+      $(e.target).addClass('ui-table-icon-state-hover');
+    },
+    removeHover: function(e) {
+      $(e.target).removeClass('ui-table-icon-state-hover');
+    },
+    setHover: function(e) {
+      e.hover(util.addHover, util.removeHover);
+    },
+    createIcon: function(name, model) {
+      return '<span class="ui-icon ui-corner-all ui-icon-' + name + '"' +
+          (model ? 'model="' + model + '"' : '')+
+          '></span>';
+    },
 
-		_init: function() {
-			var table = this;
-			var options = this.options;
+    /**
+     * Finds the widget containing an element
+     * @param e event
+     * @return Widget the event is triggered on
+     */
+    findWidget: function(e) {
+      var element = e.closest('table').parent();
+      return element.data('table');
+    }
+  };
 
-			var html = ['<table class="ui-widget" cellspacing="0"><thead class="ui-state-default"><tr>'];
-			$.each(options.columns, function(i, e) {
-				html.push('<th>', e.name, '</th>');
-			});
-			html.push('</tr></thead><tbody class="ui-widget-content">');
-			$.each(options.rows, function(r, row) {
-				html.push('<tr>');
-				$.each(options.columns, function(c, column) {
-					var cellContent = table._cell(r, row, c, column);
-					html.push(column.css ? '<td class="' + column.css + '">' : '<td>', cellContent, '</td>');
-				});
-				html.push('</tr>');
-			});
-			html.push('</tr></tbody>');
-			// html.push('<tfoot class="ui-widget-footer ui-state-default ui-corner-bl ui-corner-br"><tr><td colspan="3"><div class="ui-icon ui-corner-all ui-icon-plus"></div><div class="ui-icon ui-corner-all ui-icon-pencil"></div><div class="ui-icon ui-corner-all ui-icon-trash"></div></td></tr></tfoot>');
-			html.push('</table>');
+  var table = {
 
-			this.element.append(html.join(''));
+    _init: function() {
+      var table = this;
+      var options = this.options;
 
-			// complete styles
-			$('tbody tr:odd', this.element).addClass('ui-table-odd');
+      var html = ['<table class="ui-widget" cellspacing="0"><thead class="ui-state-default"><tr>'];
+      $.each(options.columns, function(i, e) {
+        html.push('<th>', e.name, '</th>');
+      });
+      html.push('</tr></thead><tbody class="ui-widget-content">');
+      $.each(options.rows, function(r, row) {
+        html.push('<tr>');
+        $.each(options.columns, function(c, column) {
+          var cellContent = table._cell(r, row, c, column);
+          html.push(column.css ? '<td class="' + column.css + '">' : '<td>',
+              cellContent, '</td>');
+        });
+        html.push('</tr>');
+      });
+      html.push('</tr></tbody>');
+      // html.push('<tfoot class="ui-widget-footer ui-state-default ui-corner-bl ui-corner-br"><tr><td colspan="3"><div class="ui-icon ui-corner-all ui-icon-plus"></div><div class="ui-icon ui-corner-all ui-icon-pencil"></div><div class="ui-icon ui-corner-all ui-icon-trash"></div></td></tr></tfoot>');
+      html.push('</table>');
 
-			// add event listeners
+      this.element.append(html.join(''));
 
-			// row hover
-		  $('.ui-widget-content tr', this.element).hover(function(e) {
-		  	$(e.target.parentNode).addClass('ui-state-hover');
-		  }, function(e) {
-		  	$(e.target.parentNode).removeClass('ui-state-hover');
-		  });
+      // complete styles
+      $('tbody tr:odd', this.element).addClass('ui-table-odd');
 
-		  // icons
-		  $('.ui-icon', this.element).hover(function(e) {
-		  	$(e.target).addClass('ui-state-hover');
-		  }, function(e) {
-		  	$(e.target).removeClass('ui-state-hover');
-		  })
+      // add event listeners
 
-			// pencil (edit)
-			$('.ui-icon-pencil').click(table._editCell);
-		},
+      // row hover
+      $('.ui-widget-content tr', this.element).hover(function(e) {
+        $(e.target).closest('tr').addClass('ui-table-state-hover');
+      }, function(e) {
+        $(e.target).closest('tr').removeClass('ui-table-state-hover');
+      });
 
-		_cell: function(r, row, c, column) {
-			return row[column.key] + (column.editor ?
-			    '<span class="ui-icon ui-corner-all ui-icon-pencil" model="' + r + ',' + c + '"></span>' : '');
-		},
+      // icons
+      util.setHover($('.ui-icon', this.element));
 
-		_editCell: function(e) {
-			var icon = $(e.target);
-			var element = icon.closest('table').parent();
-			var widget = element.data('table');
-			with(widget.options) { // HERE
-			  var model = icon.attr('model');
-				var rc = model.split(',');
-				var r = rc[0], c = rc[1];
-				if (columns[c].editor == 'text') {
-          console.log('model=' + rows[r][columns[c].key]);
-					var cell = icon.parent();
-					cell.html('editor <span class="ui-icon ui-corner-all ui-icon-disk" model="' + model + '"></span>');
-					$('.ui-icon-disk', cell).hover(function(e) {
-						$(e.target).addClass('ui-state-hover');
-					}, function(e) {
-						$(e.target).removeClass('ui-state-hover');
-					}).click(widget._saveCell);
-				}
-			}
-		},
+      // buttons
+      $('.ui-icon-pencil', this.element).click(table._editCell);
+    },
+
+    _cell: function(r, row, c, column) {
+      return row[column.key] +
+          (column.editor ? util.createIcon('pencil', r + ',' + c) : '');
+    },
+
+    _editCell: function(e) {
+      var icon = $(e.target);
+      var widget = util.findWidget(icon);
+      with(widget.options) {
+        var model = icon.attr('model');
+        var rc = model.split(',');
+        var r = rc[0], c = rc[1];
+        if (columns[c].editor == 'text') {
+          var cell = icon.parent();
+          cell.html('editor ' + util.createIcon('disk', model) +
+              util.createIcon('cancel', model));
+
+          util.setHover($('.ui-icon-disk', cell).click(widget._saveCell));
+          util.setHover($('.ui-icon-cancel', cell).click(widget._cancelCell));
+        }
+      }
+    },
+
+    _cancelCell: function(e) {
+      var icon = $(e.target);
+      var widget = util.findWidget(icon);
+      widget._showCell(icon, widget, function() {}); 
+    },
 
     _saveCell: function(e) {
-			var icon = $(e.target);
-			var element = icon.closest('table').parent();
-			var widget = element.data('table');
-			var cell = icon.parent();
-      var rc = icon.attr('model').split(',');
-      var r = rc[0], c = rc[1];
-			var cellContent = widget._cell(r, widget.options.rows[r], c, widget.options.columns[c]);
-			cell.html(cellContent);
-			$('.ui-icon-pencil', cell).click(widget._editCell);
-		}
+      var icon = $(e.target);
+      var widget = util.findWidget(icon);
+      widget._showCell(icon, widget, function() {
+        // save
+      }); 
+    },
 
-	};
+    _showCell: function(icon, widget, continuation) {
+      var cell = icon.parent();
+      var model = icon.attr('model').split(',');
+      var r = model[0], c = model[1];
 
-	$.widget('ui.table', prototype);
-	$.extend($.ui.table, {
-		version: '@VERSION',
-		defaults: {
-			columns: [],
-			rows: []
-		}
-	});
+      var cellContent = widget._cell(
+          r, widget.options.rows[r], c, widget.options.columns[c]);
+      cell.html(cellContent);
+
+      util.setHover($('.ui-icon-pencil', cell).click(widget._editCell));
+
+      cell.hover(function(e) {
+        $(e.target).closest('tr').addClass('ui-table-state-hover');
+      }, function(e) {
+        $(e.target).closest('tr').removeClass('ui-table-state-hover');
+      });
+    }
+
+  };
+
+  $.widget('ui.table', table);
+  $.extend($.ui.table, {
+    version: '@VERSION',
+    defaults: {
+      columns: [],
+      rows: []
+    }
+  });
 
 })(jQuery);
